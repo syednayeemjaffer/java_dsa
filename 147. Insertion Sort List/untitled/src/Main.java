7e9be423948db52894652c43cd80de1f5f36f64c
syednayeemjaffer <nayeems199@gmail.com>
2026-06-29 21:08:32 +0530

    Code

---
+++ 147. Insertion Sort List/untitled/src/Main.java
@@ -0,0 +1,33 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(5);
+        ListNode node3 = new ListNode(2);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(3);
+        ListNode node6 = new ListNode(-1);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        System.out.println("Hii");
+    }
+}
