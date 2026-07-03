15ae57846fcd4fad3ba879126f719503f0734a63
syednayeemjaffer <nayeems199@gmail.com>
2026-07-03 08:10:02 +0530

    problem solved

--- 1752. Check if Array Is Sorted and Rotated/Main/src/Main.java
+++ 1752. Check if Array Is Sorted and Rotated/Main/src/Main.java
@@ -5,33 +5,35 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int a[] = {2,1,3,4};//rot=3
+        int a[] = {3,4,5,1,2};//rot=3
         System.out.println(s.check(a));
 
     }
 }
 
+//1 2 3 4 5 6
+//2 3 4 5 6 1
+//3 4 5 6 1 2
+//4 5 6 1 2 3
+//5 6 1 2 3 4
+//6 1 2 3 4 5
+
 class Solution {
     public boolean check(int[] a) {
-        int i = 0,j=1;
-        boolean checks = false;
-        while(j<a.length){
-            if(a[i]<a[j]) {
-                i++;
-                j++;
-            }else {
-                if (!checks){
-                    checks = true;
-                    i++;
-                    j++;
-                }else {
-                    return false;
-                }
+        int count = 0;
+        for(int i = 0;i<a.length;i++){
+            if(a[i]>a[(i+1) % a.length]){
+                count++;
+            }
+
+            if(count>1){
+                return false;
             }
         }
-        return checks;
+        return true;
     }
 }
+
 //class Solution {
 //    public boolean check(int[] a) {
 //        int b [] = Arrays.copyOf(a,a.length);
